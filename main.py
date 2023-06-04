c8e3d8a2d2114db52e9ef9e3c255ce9954b7a66d
Jack Fung <fungsaulok524@gmail.com>
2023-06-04 14:09:04 +0800

    trip creation! (not working)

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from nicegui import ui, app
 import time
 import json
+import os
 
 ##init
-with open('data.json') as json_file:
-    knowndict = json.load(json_file)
+##read trips.json
+if 'trips.json' in os.listdir():
+    with open('trips.json') as json_file:
+        knowndict = json.load(json_file)
+else:
+    open('trips.json', 'w').close()
+    knowndict = ["None"]
 ###
 print("{} | data load           done".format(time.strftime("%H:%M:%S")))
 
@@ -15,17 +21,43 @@ def write_log(data):
     with open('log.txt', 'a') as f:
         f.write(data+"\n")
 
+def reload_trips():
+    with open('trips.json') as json_file:
+        knowndict = json.load(json_file)
+
+def make_trip(name):
+    ##add to trips.json
+    if name == "":
+        ui.notify("what are you trying to do")
+    ##write to trips.json (add current trips aswell (we will be overriding old file (this is really bad)))
+    else:
+        ##make name into dict
+        addtrip = [name]
+        with open('trips.json', 'w') as json_file:
+            ##check if knowndict is false ("None")
+            if knowndict == ["None"]:
+                json.dump(addtrip, json_file)
+            else:
+                knowndict.append(addtrip)
+                json.dump(knowndict, json_file)
+
 def open_trip(trip):
     if not trip == "Choose":
-        if trip in knowndict:
-            ui.open("trips/"+select1.value)
+        ##check if tripname is "None"
+        if trip == "None":
+            ui.notify("what are you trying to do")
         else:
-            ui.notify("trip not found (how)")
+            if trip in knowndict:
+                ui.open("trips/"+trip)
+            else:
+                ui.notify("trip nonexist")
     else:
-        ui.notify("select a trip (pls)")
+        ui.notify("select a trip")
 
 def update():
     timelabel1.set_text("It's currently " + time.strftime("%H:%M:%S") + " In HKT.")
+    reload_trips()
+    select1.update()
 ###
 print("{} | function load       done".format(time.strftime("%H:%M:%S")))
 
@@ -41,6 +73,7 @@ with ui.footer().style('background-color: #3874c8'):
 ##content
 with ui.tabs() as tabs:
     ui.tab('Trips', icon='flight_takeoff')
+    ui.tab('Create', icon='add_box')
     ui.tab('About', icon='info')
 
 with ui.tab_panels(tabs, value='Trips'):
@@ -49,9 +82,17 @@ with ui.tab_panels(tabs, value='Trips'):
             triplabel1 = ui.label("Select Trip:")
             select1 = ui.select(knowndict, value="Choose", on_change=lambda selected: write_log(str(selected.value)))
             loadbutton1 = ui.button("Go", on_click=lambda: open_trip(select1.value))
+    with ui.tab_panel('Create'):
+        with ui.row():
+            Creatlabel1 = ui.label("Create Trip:")
+            ##textbox
+            newtripbox = ui.input(value="", label="name:", )
+            loadbutton2 = ui.button("Go", on_click=lambda: make_trip(newtripbox.value))
     with ui.tab_panel('About'):
         ui.label('Made with pain, by Jack.')
+        ui.label('Made in Python, with NiceGUI.')
         ui.label('Project started on 4 Jun, 2023.')
+        ui.link('source code here', 'https://github.com/lokzz/Trip')
 
 ui.timer(interval=0.1, callback=lambda: update())
 
