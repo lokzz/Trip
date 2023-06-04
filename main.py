f1b50d26331172a188fb88db3dfc1cbc6c537d3d
Jack Fung <fungsaulok524@gmail.com>
2023-06-04 15:30:27 +0800

    commit

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ import time
 import json
 import os
 
-##init
-##read trips.json
+##formatting
+print('|--------------------------------------|')
+
+
 if 'trips.json' in os.listdir():
     with open('trips.json') as json_file:
         knowndict = json.load(json_file)
 else:
     open('trips.json', 'w').close()
     knowndict = ["None"]
-###
-print("{} | data load           done".format(time.strftime("%H:%M:%S")))
+print("| {} | data load          [done] |".format(time.strftime("%H:%M:%S")))
 
 
-##init end
-##functions
 def write_log(data):
     with open('log.txt', 'a') as f:
         f.write(data+"\n")
@@ -26,24 +25,19 @@ def reload_trips():
         knowndict = json.load(json_file)
 
 def make_trip(name):
-    ##add to trips.json
     if name == "":
         ui.notify("what are you trying to do")
     ##write to trips.json (add current trips aswell (we will be overriding old file (this is really bad)))
     else:
-        ##make name into dict
-        addtrip = [name]
         with open('trips.json', 'w') as json_file:
-            ##check if knowndict is false ("None")
             if knowndict == ["None"]:
-                json.dump(addtrip, json_file)
+                json.dump(name, json_file)
             else:
-                knowndict.append(addtrip)
+                knowndict.append(name)
                 json.dump(knowndict, json_file)
 
 def open_trip(trip):
     if not trip == "Choose":
-        ##check if tripname is "None"
         if trip == "None":
             ui.notify("what are you trying to do")
         else:
@@ -58,10 +52,9 @@ def update():
     timelabel1.set_text("It's currently " + time.strftime("%H:%M:%S") + " In HKT.")
     reload_trips()
     select1.update()
-###
-print("{} | function load       done".format(time.strftime("%H:%M:%S")))
+print("| {} | function load      [done] |".format(time.strftime("%H:%M:%S")))
 
-##header & footer.
+
 with ui.header().style('background-color: #3874c8').classes('items-center justify-between'):
     ui.markdown('## This is **Trip**.')
 
@@ -70,7 +63,6 @@ with ui.footer().style('background-color: #3874c8'):
     timelabel1 = ui.label("It's currently " + current_time)
 
 
-##content
 with ui.tabs() as tabs:
     ui.tab('Trips', icon='flight_takeoff')
     ui.tab('Create', icon='add_box')
@@ -85,15 +77,17 @@ with ui.tab_panels(tabs, value='Trips'):
     with ui.tab_panel('Create'):
         with ui.row():
             Creatlabel1 = ui.label("Create Trip:")
-            ##textbox
             newtripbox = ui.input(value="", label="name:", )
-            loadbutton2 = ui.button("Go", on_click=lambda: make_trip(newtripbox.value))
+            loadbutton2 = ui.button("Make", on_click=lambda: make_trip(newtripbox.value))
     with ui.tab_panel('About'):
         ui.label('Made with pain, by Jack.')
         ui.label('Made in Python, with NiceGUI.')
         ui.label('Project started on 4 Jun, 2023.')
         ui.link('source code here', 'https://github.com/lokzz/Trip')
 
-ui.timer(interval=0.1, callback=lambda: update())
+print("| {} | site content load  [done] |".format(time.strftime("%H:%M:%S")))
 
+ui.timer(interval=0.1, callback=lambda: update())
 ui.run(port=80,title="Jack's site",dark=True,language='en-US')
+print("| {} | start site         [done] |".format(time.strftime("%H:%M:%S")))
+print('|--------------------------------------|')
