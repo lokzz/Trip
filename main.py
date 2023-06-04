6e505c09d94c759b6f7422e552ef28ffdf78025c
Jack Fung <fungsaulok524@gmail.com>
2023-06-04 09:12:57 +0800

    commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 ##init
 with open('data.json') as json_file:
     knowndict = json.load(json_file)
+###
 print("{} | data load           done".format(time.strftime("%H:%M:%S")))
 
 
@@ -14,14 +15,28 @@ def write_log(data):
     with open('log.txt', 'a') as f:
         f.write(data+"\n")
 
+def open_trip(trip):
+    if not trip == "Choose":
+        if trip in knowndict:
+            ui.open("trips/"+select1.value)
+        else:
+            ui.notify("trip not found (how)")
+    else:
+        ui.notify("select a trip (pls)")
+
 def update():
     timelabel1.set_text("It's currently " + time.strftime("%H:%M:%S") + " In HKT.")
+###
+print("{} | function load       done".format(time.strftime("%H:%M:%S")))
 
-##header & time
-ui.markdown('## This is **Trip**.')
+##header & footer.
+with ui.header().style('background-color: #3874c8').classes('items-center justify-between'):
+    ui.markdown('## This is **Trip**.')
 
-current_time = time.strftime("%H:%M:%S")
-timelabel1 = ui.label("It's currently " + current_time)
+with ui.footer().style('background-color: #3874c8'):
+    current_time = time.strftime("%H:%M:%S")
+    timelabel1 = ui.label("It's currently " + current_time)
+
 
 ##content
 with ui.tabs() as tabs:
@@ -30,14 +45,10 @@ with ui.tabs() as tabs:
 
 with ui.tab_panels(tabs, value='Trips'):
     with ui.tab_panel('Trips'):
-        ##put in row
         with ui.row():
-            ##align label to middle
             triplabel1 = ui.label("Select Trip:")
-            select1 = ui.select(knowndict, value=1, on_change=lambda selected: write_log(str(selected.value)))
-            select1val = str(select1.value)
-            ##load button (link)
-            loadbutton1 = ui.button("Go", on_click=lambda: ui.open("trips/"+select1val))
+            select1 = ui.select(knowndict, value="Choose", on_change=lambda selected: write_log(str(selected.value)))
+            loadbutton1 = ui.button("Go", on_click=lambda: open_trip(select1.value))
     with ui.tab_panel('About'):
         ui.label('Made with pain, by Jack.')
         ui.label('Project started on 4 Jun, 2023.')
