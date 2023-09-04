3aef75a487d9a695ec3bac44a61fc814f0960d0c
lokzz <50368061+lokzz@users.noreply.github.com>
2023-09-05 01:05:20 +0800

    d

--- main.py
+++ main.py
@@ -59,15 +59,23 @@ def make_spot(name):
         with open('spots.json', 'w') as json_file:
             try:
                 open("trips/"+name+".json", "x")
+                newtrip = open("trips/"+name+".json", "w")
                 if knowndict == ["None"]:
                     json.dump(name, json_file)
                 else:
                     knowndict.append(name)
                     json.dump(knowndict, json_file)
-                ui.open("spots?name=_"+str(select1.value))
+                defaultdict = {"name": name, "date": "", "estarrival": "", "location": "", "description": ""}
+                json.dump(defaultdict, newtrip)
+                ui.open("spots?name=_"+str(name))
             except:
                 ui.notify(f"spot \"{name}\" already exists.")
                 tabs.set_value('spots')
+
+def del_spot(name):
+    knowndict.remove(name)
+    os.remove("trips/"+name+".json")
+    ui.notify("spot deleted")
 
 def open_spot(spot):
     spot = str(spot)
@@ -79,7 +87,7 @@ def open_spot(spot):
             if spot in knowndict:
                 ui.open("spots?name=_"+str(select1.value))
             else:
-                ui.notify("spot nonexist")
+                ui.notify("spot does not exist")
     else:
         ui.notify("select a spot")
 
@@ -100,20 +108,24 @@ with ui.footer().style('background-color: #3874c8'):
     timelabel1 = ui.label("It's currently " + current_time)
 
 with ui.tabs() as tabs:
-    ui.tab('spots', icon='flight_takeoff')
-    ui.tab('Create', icon='add_box')
+    spot = ui.tab('spots', icon='flight_takeoff')
+    ui.tab('modify', icon='add_box')
     ui.tab('About', icon='info')
-with ui.tab_panels(tabs, value='spots'):
+with ui.tab_panels(tabs, value=spot):
     with ui.tab_panel('spots'):
         with ui.row():
             spotlabel1 = ui.label("Select spot:")
             select1 = ui.select(knowndict, value="None", on_change=lambda selected: write_log("[" + str(selected.value) + "]" + " chosen"))
             loadbutton1 = ui.button("Go", on_click=lambda: open_spot(select1.value))
-    with ui.tab_panel('Create'):
+    with ui.tab_panel('modify'):
         with ui.row():
             Creatlabel1 = ui.label("Create spot:")
             newspotbox = ui.input(value="", label="name:").on('keydown.enter', lambda: make_spot(newspotbox.value))
             loadbutton2 = ui.button("Make", on_click=lambda: make_spot(newspotbox.value))
+        with ui.row():
+            Deletlabel1 = ui.label("Delete spot:")
+            deletspotbox = ui.select(knowndict, value="None")
+            loadbutton3 = ui.button("Delete", on_click=lambda: del_spot(deletspotbox.value))
     with ui.tab_panel('About'):
         ui.label('Made with pain, by Jack.')
         ui.label('Made in Python, with NiceGUI.')
@@ -126,6 +138,14 @@ with ui.tab_panels(tabs, value='spots'):
 def spots(request: Request):
     def update():
         timelabel1.set_text("It's currently " + time.strftime("%H:%M:%S") + " In HKT.")
+    def save(oldjson, name, date, estarrival, loc, desc, realname):
+        oldjson["name"] = name
+        oldjson["date"] = date
+        oldjson["estarrival"] = estarrival
+        oldjson["location"] = loc
+        oldjson["description"] = desc
+        json.dump(oldjson, open("trips/"+realname+".json", "w"))
+        write_log("writing new data to " + realname)
     
     with ui.header().style('background-color: #3874c8'):
         with ui.column():
@@ -138,9 +158,15 @@ def spots(request: Request):
     requestedspot = str(request._query_params)
     requestedspot = requestedspot.replace("name=_", "")
     spotheader.set_content("Chosen spot: **{}**".format(requestedspot))
-    requestedspot_json = requestedspot+".json"
-    if requestedspot_json in os.listdir("trips"):
+    if (requestedspot+".json") in os.listdir("trips"):
+        spotjson = json.load(open("trips/"+requestedspot+".json"))
         write_log("[" + requestedspot + "]" +" called")
+        spotname = ui.input(value=spotjson["name"], label="name:")
+        spotdate = ui.input(value=spotjson["date"], label="date:")
+        spotarriv = ui.input(value=spotjson["estarrival"], label="arrive at:")
+        spotloc = ui.input(value=spotjson["location"], label="location:")
+        spotdesc = ui.input(value=spotjson["description"], label="description:")
+        ui.button("Save", on_click=lambda: save(spotjson, spotname.value, spotdate.value, spotarriv.value, spotloc.value, spotdesc.value, requestedspot))
     else:
         ui.label("spot not found.")
         ui.button("GO BACK", on_click=lambda: ui.open("/"))
